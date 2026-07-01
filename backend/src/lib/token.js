ac43c1926034a8df2bfb8dc98a51c8b33f6282fc
ASHIKA22R <rashika051@gmail.com>
2026-07-01 22:53:52 +0530

    commit change

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -1,10 +1,18 @@
-import jwt from "jsonwebtoken"
-const  tokenGeneration  =(userId,res)=>{
-    const  token=jwt.sign({userId},process.env.secretkey,{expiresIn:"7d",})
-    res.cookie("jwt",token,{
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-        sameSite:"strict",
-    });
+import jwt from "jsonwebtoken";
+
+const tokenGeneration = (userId, res) => {
+  const token = jwt.sign(
+    { userId },
+    process.env.secretkey,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 };
-export default tokenGeneration ;
+
+export default tokenGeneration;
