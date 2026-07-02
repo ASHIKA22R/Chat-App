fcccfe8abf753b0d9d36e86e70eb40c71d7d7439
ASHIKA22R <rashika051@gmail.com>
2026-07-02 08:13:22 +0530

    commit change

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,17 +7,27 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-  origin: ["http://localhost:5173", "https://chat-app-httpashika.vercel.app"],
-  credentials: true
-}));
+// Increase request body size limit
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-const port = process.env.PORT;
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-httpashika.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
 
 import authRoute from "./routes/authRoute.js";
 import messageRoute from "./routes/messageRoute.js";
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
@@ -25,10 +35,15 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
-mongoose.connect(process.env.MONGODB_URI).then((res) => {
-  console.log("mongoDB connected:" + res.connection.host);
-  server.listen(port, () => {
-    console.log(`server running on port ${port}`);
-  });
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((res) => {
+    console.log("mongoDB connected:", res.connection.host);
 
+    server.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
